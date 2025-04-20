76ac976cf0b195703948aa2c9a8f71313a20afb2
Isacescu Maxim <73758515+M1XaM@users.noreply.github.com>
2025-04-20 13:36:00 +0300

    Update isacescu_maxim_gambler_round_2.py

--- isacescu_maxim_gambler_round_2.py
+++ isacescu_maxim_gambler_round_2.py
@@ -1,4 +1,4 @@
-def strategy_round_3(opponent_id, my_history, opponents_history):
+def strategy_round_2(opponent_id, my_history, opponents_history):
     interaction_count = len(my_history.get(opponent_id, []))
     seed = opponent_id * 12345 + interaction_count
     move = (seed * 5144013 + 22310311) % 2
